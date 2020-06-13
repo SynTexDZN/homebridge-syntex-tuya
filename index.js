dac1e6a2cccbd1c85327751760bfb8f65d53b1ab
SynTexDZN <syntexdzn@t-online.de>
2020-06-13 03:11:59 +0200

    improvements

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ function SynTexSwitchAccessory(id, name)
     this.id = id;
     this.name = name;
 
-    this.service = new Service.Switch(this.name);
+    this.service = new Service.Outlet(this.name);
     /*
     DeviceManager.getDevice(this).then(function(state) {
 
