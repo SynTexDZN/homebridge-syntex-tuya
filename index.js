e953d46ba0d4fa075cde9c5653e459e9337927fb
SynTex <syntexdzn@t-online.de>
2020-12-15 19:49:25 +0100

    Accessory Removal Fixes

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ class SynTexTuyaPlatform extends DynamicPlatform
 				{
 					if(urlParams.remove == 'CONFIRM')
 					{
-						this.removeAccessory(accessory);
+						this.removeAccessory(accessory.homebridgeAccessory);
 					}
 
 					response.write(urlParams.remove == 'CONFIRM' ? 'Success' : 'Error');
